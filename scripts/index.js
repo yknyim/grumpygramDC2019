bf1dc23f82c14d3291f5c05b2d830b0d21841a7e
Ryan Yim <yknyim@gmail.com>
2019-02-28 11:59:15 -0500

    adds big function

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,15 @@ const targetElements = document.querySelectorAll("[data-target]");
 
 // We see an enlarged version of image that is inside that .picture-frame
 
-function respondToClick() {
+function respondToClick(event) {
     console.log('hello');
     console.log('how are you?');
     console.log('i am fine because i am a hamster.');
+
+    console.log(event.target.parentElement);
+    // add the "big" class tot he thing that got clicked
+
+    event.target.parentElement.classList.add('big');
 }
 
 function attachClickHandler(oneElement) {
