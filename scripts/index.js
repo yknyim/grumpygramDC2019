51a96df2ddcb2ba35117087e46c7e7d23c104ee7
Ryan Yim <yknyim@gmail.com>
2019-02-28 11:26:32 -0500

    adds click handler function

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+console.log("is this thing on? hello?");
+
+// When we click any.picture-frame
+// A const is a "constant" variable which means
+// that is cannot be reassigned.
+const targetElements = document.querySelectorAll("[data-target]");
+
+// We see an enlarged version of image that is inside that .picture-frame
+
+function respondToClick() {
+    console.log('hello');
+    console.log('how are you?');
+    console.log('i am fine because i am a hamster.');
+}
+
+targetElements[0].addEventListener("click", respondToClick);
